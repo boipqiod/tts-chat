6d1f1aefd187aa6f3a6ffc8c651429d11ba1c2ea
simon <simon@eximbay.com>
2024-08-26 08:54:29 +0900

    fix. 컨피그 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+    host: '0.0.0.0',
+    port: 10000, // 원하는 포트로 변경 가능
+  },
 });
